810a1ae5bdaa2c38c8c7d1fb36f7af13cea32c38
RuiOliveira83 <rui.p3ld@gmail.com>
2021-09-07 09:01:33 +0000

    Added class methods to webhook handler and webhook view

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,13 +1,14 @@
 /*
     Core logic/payment flow for this comes from here:
     https://stripe.com/docs/payments/accept-a-payment
-    CSS from here: 
+
+    CSS from here:
     https://stripe.com/docs/stripe-js
 */
 
-var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
-var clientSecret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripePublicKey);
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripe_public_key);
 var elements = stripe.elements();
 var style = {
     base: {
@@ -43,36 +44,49 @@ card.addEventListener('change', function (event) {
     }
 });
 
-// Handle form submit
+// Handle from submit
 var form = document.getElementById('payment-form');
 
-form.addEventListener('submit', function(ev) {
-    ev.preventDefault();
-    card.update({ 'disabled': true});
-    $('#submit-button').attr('disabled', true);
-    $('#payment-form').fadeToggle(100);
-    $('#loading-overlay').fadeToggle(100);
-    stripe.confirmCardPayment(clientSecret, {
-        payment_method: {
-            card: card,
-        }
-    }).then(function(result) {
-        if (result.error) {
-            var errorDiv = document.getElementById('card-errors');
-            var html = `
-                <span class="icon" role="alert">
-                <i class="fas fa-times"></i>
-                </span>
-                <span>${result.error.message}</span>`;
-            $(errorDiv).html(html);
-            $('#payment-form').fadeToggle(100);
-            $('#loading-overlay').fadeToggle(100);
-            card.update({ 'disabled': false});
-            $('#submit-button').attr('disabled', false);
-        } else {
-            if (result.paymentIntent.status === 'succeeded') {
-                form.submit();
-            }
-        }
-    });
+var resultContainer = document.getElementById('payment-result');
+cardElement.on('change', function(event) {
+  if (event.error) {
+    resultContainer.textContent = event.error.message;
+  } else {
+    resultContainer.textContent = '';
+  }
 });
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  resultContainer.textContent = "";
+  stripe.createPaymentMethod({
+    type: 'card',
+    card: cardElement,
+  }).then(handlePaymentMethodResult);
+});
+
+function handlePaymentMethodResult(result) {
+  if (result.error) {
+    // An error happened when collecting card details, show it in the payment form
+    resultContainer.textContent = result.error.message;
+  } else {
+    // Otherwise send paymentMethod.id to your server (see Step 3)
+    fetch('/pay', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ payment_method_id: result.paymentMethod.id })
+    }).then(function(result) {
+      return result.json();
+    }).then(handleServerResponse);
+  }
+}
+
+function handleServerResponse(responseJson) {
+  if (responseJson.error) {
+    // An error happened when charging the card, show it in the payment form
+    resultContainer.textContent = responseJson.error;
+  } else {
+    // Show a success message
+    resultContainer.textContent = 'Success!';
+  }
+}
