4bf07f46bb83e5d7ed010bb9813b10e53c40838c
RuiOliveira83 <rui.p3ld@gmail.com>
2021-09-19 21:37:18 +0000

    Added cache control

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -182,6 +182,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+     # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'ro8780-boutique-ado'
     AWS_S3_REGION_NAME = 'eu-north-1'
@@ -203,6 +209,7 @@ if 'USE_AWS' in os.environ:
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
